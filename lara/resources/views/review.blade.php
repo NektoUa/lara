343fe0f1a0306067297290e44d687084c2079512
NektoUa <illyabarkov@gmail.com>
2022-01-11 10:04:53 +0200

    add

--- lara/resources/views/review.blade.php
+++ lara/resources/views/review.blade.php
@@ -4,38 +4,44 @@
 @endsection
 
 @section("main_content")
-<h1>Please write Review</h1>
+<div class="container">
+    <h1>Please write Review</h1>
 
-@if($errors->any())
-<div>
-    <ul>
-        @foreach($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
+    @if($errors->any())
+    <div>
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <form method="POST" action="/review/check" class="form-group">
+        @csrf
+        <label for="email1">Email address</label>
+        <input type="email" name="email" id="email" placeholder="Your e-mail" class="form-control">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <br>
+        <br>
+        <label for="subject">Subject</label>
+        <input type="subject" name="subject" id="subject" placeholder="Your sub" class="form-control">
+        <br>
+        <label for="message">Your review</label>
+        <textarea name="message" id="message" placeholder="Your reviwiviwi" class="form-control"></textarea>
+        <br>
+        <button type="submit" class="btn btn-primary">Subimitti</button>
+    </form>
+    <br>
+    <h2>Інші відгуки</h2>
+    @foreach($rew as $elPaso)
+    <div class="row">
+        <div class="col">{{$elPaso->subject}}</div>
+        <div class="col-6">{{$elPaso->message}}</div>
+        <div class="col">{{$elPaso->id}}</div>
+    </div>
+    @endforeach
+    <div></div>
+    <button class="btn btn-dark"><a href="/">return</a></button>
 </div>
-@endif
-
-<form method="POST" action="/review/check" class="form-group">
-    @csrf
-    <input type="email" name="email" id="email" placeholder="Your e-mail" class="form-control">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    <br>
-    <br>
-    <input type="subject" name="subject" id="subject" placeholder="Your sub" class="form-control">
-    <br>
-
-    <textarea name="message" id="message" placeholder="Your reviwiviwi" class="form-control"></textarea>
-    <br>
-    <button type="submit" class="btn btn-primary">Subimitti</button>
-</form>
-<br>
-<h1>Інші відгуки</h1>
-@foreach($rew as $elPaso)
-<h3>{{$elPaso->subject}}</h3>
-<span>{{$elPaso->id}}</span>
-<p>{{$elPaso->message}}</p>
-@endforeach
-<div></div>
-<button class="btn btn-dark"><a href="/">return</a></button>
 @endsection
